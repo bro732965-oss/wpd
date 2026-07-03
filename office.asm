260a42aa24685a63b24f4f442dfd6421e694b178
Fedor <bro732965@gmail.com>
2026-07-03 21:25:05 +0500

    Update office.asm

--- office.asm
+++ office.asm
@@ -5,7 +5,7 @@
     cmd db 20, 0, 20 dup('$')
     bird db 20, 0, 20 dup('$')
     runs db 20, 0, 20 dup('$')
-    runing db 'program.exe', 0
+    runing db 20 dup('$'), 0    ; ← изменено
     brick db 0
     ask db 0
     name db 20, 0, 20 dup('$')
@@ -13,6 +13,9 @@
     text db 20, 0, 20 dup('$')
     len db 0
     handle dw ?
+    pl db 0
+    pi db 0
+    col db 0
     msg1 db 'Message 1$'
     msg2 db 'Message 2$'
     msg3 db 'Message 3$'
@@ -29,35 +32,35 @@ s1:
 
     cmp [cmd], '1'
     je  driver1
-
     cmp [cmd], '2'
     je  run
-
     cmp [cmd], '3'
     je  msgs1
-
     cmp [cmd], '4'
     je  msgs2
-
     cmp [cmd], '5'
     je  msgs3
-
     cmp [cmd], '6'
     je  read
-
     cmp [cmd], '7'
     je  as
-
     cmp [cmd], '8'
     je  w
-
     cmp [cmd], '9'
     je  r
-
     cmp [cmd], 'a'
     je  rec
+    cmp [cmd], 'b'
+    je  pos
+    cmp [cmd], 'c'
+    je  g
+    cmp [cmd], 't'
+    je  on
+    cmp [cmd], 'o'
+    je  rauning
+    cmp [cmd], 'n'              ; ← добавлено
+    je  rename                  ; ← добавлено
 
-    ; Если ни одна не подошла
     mov ah, 09h
     mov dx, offset err_msg
     int 21h
@@ -76,61 +79,30 @@ run:
     int 21h
     jmp s1
 
-msgs1:
-    mov ah, 09h
-    mov dx, offset msg1
-    int 21h
-    jmp s1
+; ... (остальные команды без изменений) ...
 
-msgs2:
-    mov ah, 09h
-    mov dx, offset msg2
-    int 21h
-    jmp s1
-
-msgs3:
-    mov ah, 09h
-    mov dx, offset msg3
-    int 21h
-    jmp s1
-
-as:
+rauning:
     mov ah, 01h
     int 21h
-    mov [brick], al
+    mov [runing], al
     jmp s1
 
-read:
-    mov ah, 09h
-    mov dx, offset brick
+; --- Новая команда rename ---
+rename:                         ; ← добавлено
+    mov ah, 0Ah
+    mov dx, offset runs
     int 21h
-    jmp s1
 
-; --- Создание файла ---
-r:
-    mov ah, 3Ch
-    mov cx, 0
-    mov dx, offset filename
-    int 21h
-    mov [handle], ax
-    jmp s1
+    mov si, offset runs + 2
+    mov di, offset runing
+copy_loop:
+    mov al, [si]
+    mov [di], al
+    inc si
+    inc di
+    cmp al, 13
+    jne copy_loop
 
-; --- Запись в файл ---
-w:
-    mov ah, 40h
-    mov bx, [handle]
-    mov cx, 10
-    mov dx, offset text
-    int 21h
-    jmp s1
-
-; --- Чтение файла ---
-rec:
-    mov ah, 3Fh
-    mov bx, [handle]
-    mov cx, 10
-    mov dx, offset text
-    int 21h
     jmp s1
 
 end s1
