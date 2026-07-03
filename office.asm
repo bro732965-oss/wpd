637dedd9e67187c554164c99f514463578ce7a60
Fedor <bro732965@gmail.com>
2026-07-03 23:13:56 +0500

    Update office.asm

--- office.asm
+++ office.asm
@@ -2,107 +2,126 @@
 .stack 100h
 
 .data
-    cmd db 20, 0, 20 dup('$')
-    bird db 20, 0, 20 dup('$')
-    runs db 20, 0, 20 dup('$')
-    runing db 20 dup('$'), 0    ; ← изменено
-    brick db 0
-    ask db 0
-    name db 20, 0, 20 dup('$')
-    filename db 'test.txt', 0
-    text db 20, 0, 20 dup('$')
-    len db 0
-    handle dw ?
-    pl db 0
-    pi db 0
-    col db 0
-    msg1 db 'Message 1$'
-    msg2 db 'Message 2$'
-    msg3 db 'Message 3$'
+    a db 20, 0, 20 dup('$')
+    box db 0
+    box1 db 0
+    com1 db 0
+    com2 db 0
+    drv db 0           
+    Flag db 0
+    msg1 db 'Driver 1$'
+    msg2 db 'Driver 2$'
     err_msg db 'none$'
+    
+    ; Данные для файла
+    filename db 'file.txt', 0
+    buffer db 0, 0, 0   ; место для Flag, drv, box
+    handle dw ?
 
 .code
-s1:
+sp1:
     mov ax, @data
     mov ds, ax
 
     mov ah, 01h
     int 21h
-    mov [cmd], al
+    mov [a], al
 
-    cmp [cmd], '1'
-    je  driver1
-    cmp [cmd], '2'
-    je  run
-    cmp [cmd], '3'
-    je  msgs1
-    cmp [cmd], '4'
-    je  msgs2
-    cmp [cmd], '5'
-    je  msgs3
-    cmp [cmd], '6'
+    cmp [a], '1'
+    je  driver
+
+    cmp [a], '2'
+    je  driver2
+
+    cmp [a], '3'
     je  read
-    cmp [cmd], '7'
-    je  as
-    cmp [cmd], '8'
-    je  w
-    cmp [cmd], '9'
-    je  r
-    cmp [cmd], 'a'
-    je  rec
-    cmp [cmd], 'b'
-    je  pos
-    cmp [cmd], 'c'
-    je  g
-    cmp [cmd], 't'
-    je  on
-    cmp [cmd], 'o'
-    je  rauning
-    cmp [cmd], 'n'              ; ← добавлено
-    je  rename                  ; ← добавлено
+
+    cmp [a], '4'
+    je  com
+
+    cmp [a], '5'
+    je  flag
+
+    cmp [a], '6'        ; ← добавлена команда export
+    je  export
 
     mov ah, 09h
     mov dx, offset err_msg
     int 21h
-    jmp s1
+    jmp sp1
 
-driver1:
-    mov ah, 09h
-    mov dx, offset msg1
-    int 21h
-    jmp s1
-
-run:
-    mov ah, 4Bh
-    mov al, 0
-    mov dx, offset runing
-    int 21h
-    jmp s1
-
-; ... (остальные команды без изменений) ...
-
-rauning:
+driver:
     mov ah, 01h
     int 21h
-    mov [runing], al
-    jmp s1
+    mov [box], al
+    mov ax, [box]
+    jmp sp1
 
-; --- Новая команда rename ---
-rename:                         ; ← добавлено
-    mov ah, 0Ah
-    mov dx, offset runs
+driver2:
+    mov ah, 01h
+    int 21h
+    mov [box1], al
+    mov bx, [box1]
+    jmp sp1
+
+read:
+    mov bx, @data
+    jmp sp1
+
+com:
+    mov ah, 01h
+    int 21h
+    mov [com1], al
+
+    mov ah, 01h
+    int 21h
+    mov [com2], al
+
+    mov al, [com1]
+    mov bl, [com2]
+    cmp al, bl
+
+    je  equal
+    mov [drv], 0     
+    jmp sp1
+
+equal:
+    mov [drv], 1        
+    jmp sp1
+
+flag:
+    mov ax, @data       
+    mov [Flag], al        
+    jmp sp1
+
+export:
+    ; Копируем данные в буфер
+    mov al, [Flag]
+    mov [buffer], al
+    mov al, [drv]
+    mov [buffer + 1], al
+    mov al, [box]
+    mov [buffer + 2], al
+
+    ; Создаём файл
+    mov ah, 3Ch
+    mov cx, 0
+    mov dx, offset filename
+    int 21h
+    mov [handle], ax
+
+    ; Записываем в файл
+    mov ah, 40h
+    mov bx, [handle]
+    mov cx, 3
+    mov dx, offset buffer
     int 21h
 
-    mov si, offset runs + 2
-    mov di, offset runing
-copy_loop:
-    mov al, [si]
-    mov [di], al
-    inc si
-    inc di
-    cmp al, 13
-    jne copy_loop
+    ; Закрываем файл
+    mov ah, 3Eh
+    mov bx, [handle]
+    int 21h
 
-    jmp s1
+    jmp sp1
 
-end s1
+end sp1
