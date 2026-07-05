2bfc84f6c324a707177a00eaec210102601568a2
Fedor <bro732965@gmail.com>
2026-07-05 22:58:13 +0500

    Update office.asm

--- office.asm
+++ office.asm
@@ -3,22 +3,21 @@
 
 .data
     cmd db 0, 20, 0 dup('$')
-    a1 db '0'
-    a2 db '0'
-    a3 db '1'
-    a4 db '0'
-    box db '0'
-    msg_ok db '[ok]$'
-    filename db 'save.dat', 0
-    handle dw ?
-    buffer db 4 dup('$')
+    a db '0'
+    b db '0'
+    b1 db '0'
+    b2 db '0'
+    x db '0'
+    ab db 'off'
+    b3 db '0'
+    d1 db 0
+    d2 db 0
 
 .code
-start:
+S:
     mov ax, @data
     mov ds, ax
 
-A:
     mov ah, 01h
     int 21h
     mov [cmd], al
@@ -27,147 +26,52 @@ A:
     je  driver1
 
     cmp [cmd], '2'
-    je  driver2
+    je  video
 
     cmp [cmd], '3'
-    je  driver3
+    je  ab4
 
-    cmp [cmd], '4'
-    je  driver4
+    jmp S
 
-    cmp [cmd], '5'
-    je  export_data
+ab4:
+    cmp bx, cx
+    mov [x], al
+    jmp S
 
-    cmp [cmd], '6'
-    je  import_data
-
-    cmp [cmd], 't'
-    je  test
-
-    jmp A
-
-; --- driver1: ввод данных ---
-driver1:
-    mov ah, 01h
-    int 21h
-    mov [a1], al
-    jmp A
-
-; --- driver2: графика (синий квадрат) ---
-driver2:
+video:
     mov ax, 0x0013
     int 0x10
     mov ax, 0xA000
     mov es, ax
-    mov di, 0
-    mov cx, 2500
-    mov al, 1
-    rep stosb
-    mov ah, 0x00
-    int 0x16
-    jmp A
+    jmp S
 
-; --- driver3: звук (beep) ---
-driver3:
-    mov al, 0B6h
-    out 43h, al
-    mov ax, 4560
-    out 42h, al
-    mov al, ah
-    out 42h, al
-    in al, 61h
-    or al, 00000011b
-    out 61h, al
-    mov cx, 1000
-delay:
-    loop delay
-    in al, 61h
-    and al, 11111100b
-    out 61h, al
-    jmp A
-
-; --- driver4: вывод ---
-driver4:
-    mov ah, 09h
-    mov dx, offset a1
+driver1:
+    mov ah, 01h
     int 21h
-    jmp A
+    cmp [cmd], '1'
+    je  ab1
+    cmp [cmd], '2'
+    je  ab2
+    cmp [cmd], '3'
+    je  ab3
+    jmp S
 
-; --- export: сохранение данных в файл ---
-export_data:
-    ; Собираем данные в буфер
-    mov al, [a1]
-    mov [buffer], al
-    mov al, [a2]
-    mov [buffer+1], al
-    mov al, [a3]
-    mov [buffer+2], al
-    mov al, [a4]
-    mov [buffer+3], al
-
-    ; Создаём файл
-    mov ah, 3Ch
-    mov cx, 0
-    mov dx, offset filename
+ab1:
+    mov ah, 01h
     int 21h
-    mov [handle], ax
+    mov [a], al
+    mov bx, [a]
+    jmp S
 
-    ; Записываем данные
-    mov ah, 40h
-    mov bx, [handle]
-    mov cx, 4
-    mov dx, offset buffer
+ab2:
+    mov ah, 01h
     int 21h
+    mov [b], al
+    mov cx, [b]
+    jmp S
 
-    ; Закрываем файл
-    mov ah, 3Eh
-    mov bx, [handle]
+ab3:
+    mov ah, 01h
     int 21h
-
-    jmp A
-
-; --- import: загрузка данных из файла ---
-import_data:
-    ; Открываем файл
-    mov ah, 3Dh
-    mov al, 0
-    mov dx, offset filename
-    int 21h
-    mov [handle], ax
-
-    ; Читаем данные
-    mov ah, 3Fh
-    mov bx, [handle]
-    mov cx, 4
-    mov dx, offset buffer
-    int 21h
-
-    ; Закрываем файл
-    mov ah, 3Eh
-    mov bx, [handle]
-    int 21h
-
-    ; Загружаем данные из буфера в переменные
-    mov al, [buffer]
-    mov [a1], al
-    mov al, [buffer+1]
-    mov [a2], al
-    mov al, [buffer+2]
-    mov [a3], al
-    mov al, [buffer+3]
-    mov [a4], al
-
-    jmp A
-
-; --- test: запуск всех команд по очереди ---
-test:
-    call driver1
-    call driver2
-    call driver3
-    call driver4
-    mov ah, 09h
-    mov dx, offset msg_ok
-    int 21h
-    jmp A
-
-end start
+    mov [b], al
+    jmp S
