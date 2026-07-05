b18bf0dfd801b2e1f76b711018a2c4248da002bd
Fedor <bro732965@gmail.com>
2026-07-05 22:59:08 +0500

    Update office.asm

--- office.asm
+++ office.asm
@@ -5,13 +5,7 @@
     cmd db 0, 20, 0 dup('$')
     a db '0'
     b db '0'
-    b1 db '0'
-    b2 db '0'
     x db '0'
-    ab db 'off'
-    b3 db '0'
-    d1 db 0
-    d2 db 0
 
 .code
 S:
@@ -29,49 +23,66 @@ S:
     je  video
 
     cmp [cmd], '3'
-    je  ab4
+    je  compare
 
     jmp S
 
-ab4:
-    cmp bx, cx
-    mov [x], al
+; --- Сравнение bx и cx ---
+compare:
+    mov al, [a]
+    mov bl, [b]
+    cmp al, bl
+    je  equal
+    mov [x], '0'
     jmp S
 
+equal:
+    mov [x], '1'
+    jmp S
+
+; --- Графика ---
 video:
     mov ax, 0x0013
     int 0x10
     mov ax, 0xA000
     mov es, ax
+    mov di, 0
+    mov cx, 2500
+    mov al, 1
+    rep stosb
     jmp S
 
+; --- Меню драйверов ---
 driver1:
     mov ah, 01h
     int 21h
     cmp [cmd], '1'
-    je  ab1
+    je  input_a
+
     cmp [cmd], '2'
-    je  ab2
+    je  input_b
+
     cmp [cmd], '3'
-    je  ab3
+    je  show_result
+
     jmp S
 
-ab1:
+input_a:
     mov ah, 01h
     int 21h
     mov [a], al
-    mov bx, [a]
     jmp S
 
-ab2:
-    mov ah, 01h
-    int 21h
-    mov [b], al
-    mov cx, [b]
-    jmp S
-
-ab3:
+input_b:
     mov ah, 01h
     int 21h
     mov [b], al
     jmp S
+
+show_result:
+    mov ah, 09h
+    mov dx, offset x
+    int 21h
+    jmp S
+
+end S
